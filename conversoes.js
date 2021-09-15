a44d56b8544236164b9f3ed3a5a7a8a98ca4b8f2
Elielson Natan Ferreira da Silva <elielsonnatanferreira@gmail.com>
2021-09-15 11:48:09 -0300

    "Nova alteração no arquivo conversoes"

--- conversoes.js
+++ conversoes.js
@@ -7,3 +7,8 @@ console.log(numero + numeroString)
 
 // CONVERSÃO EXPLÍCITA
 console.log(numero + Number(numeroString))
+
+const numero2 = 456;
+const numeroStringConvertido = Number("456");
+
+console.log(numero2 + numeroStringConvertido)
