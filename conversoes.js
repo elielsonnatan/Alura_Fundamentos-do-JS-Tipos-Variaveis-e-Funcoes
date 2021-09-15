bf71d9c3bf7a772152e65cc6a8b6923091ccc797
Elielson Natan Ferreira da Silva <elielsonnatanferreira@gmail.com>
2021-09-15 11:45:01 -0300

    Novos códigos - var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,9 @@
+// CONVERSÃO IMPLÍCITA
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString)
+console.log(numero + numeroString)
+
+// CONVERSÃO EXPLÍCITA
+console.log(numero + Number(numeroString))
---
+++ null-undefined.js
@@ -0,0 +1,9 @@
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
